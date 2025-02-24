0ca52c9ef04ad5c72c8a52368914108dcd07e1d3
Guilleg8 <ggarcpey@myuax.com>
2025-02-24 18:36:45 +0100

    Matematicas(a medias)
    Creo el main y la clase matematicas que es la que genera el numero pi, se queda a medias

---
+++ Matematicas.java
@@ -0,0 +1,11 @@
+public class Matematicas {
+    private int puntosTotales;
+    public static double generarNumeroPi(long pasos){
+        int aciertos = 0;
+        int areaCuadrado = 4;
+        for (int i = 0; i < pasos; i++) {
+            
+        }
+        return 0;
+    }
+}
